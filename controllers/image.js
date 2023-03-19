c55145d850e0422d4bdcc03b27251f76eb76da80
Liam <helm_of_protection@hotmail.com>
2023-03-18 18:26:57 -0800

    fix

--- controllers/image.js
+++ controllers/image.js
@@ -7,9 +7,9 @@ const app = new Clarifai.App({
 
 //Sends request to Clarifai api
 function handleApiCall(req, res) {
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-    .then(data => res.json(data))
-    .catch(err => res.status(400).json('Unable to call Api'))
+    app.models.predict('face-detection', req.body.input)
+        .then(data => res.json(data))
+        .catch(err => res.status(400).json('Unable to call Api'))
 }
 
 //Updates counter in database for user making request
